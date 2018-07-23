000d03d06456517e237d63e3a465dad6978de58f
cgarbin <garbin.christian@gmail.com>
2018-07-23 13:32:37 -0400

    First working version
    stdin has to be explicitly invoked with '-'. Need to change
    to default to stdin when called without parameters.

---
+++ README.md
@@ -0,0 +1,45 @@
+# seed-command-line-arg-parsing
+
+A seed for processing files in JavaScript.
+
+Covers:
+
+- Process more than one file with file globbing
+- Working with pipes (redirect stdin and stdout)
+- Accepting command line options with file names
+- -Mutually exclusive command line options with a default option
+
+Sources for this code and command-line programs in general:
+
+- https://nodejs.org/api/readline.html
+- https://nodejs.org/api/readline.html#readline_example_read_file_stream_line_by_line
+- Discussion in [StackOverflow](https://stackoverflow.com/questions/6156501/read-a-file-one-line-at-a-time-in-node-js),
+  especially about how **not** do it.
+- [The command line parsing seed program](https://github.com/cgarbin/seed-javascript-command-line-arg-parsing)
+
+Examples:
+
+```
+file.txt --tolower
+file.txt --toupper
+file.txt (defaults to '--tolower')
+*.txt --tolower  (file globbing)
+cat file.txt | thisscript.py (read from stdin)
+```
+
+It's also meant to be installed as with node (`npm install -g`). See the `bin` section
+of [package.json](package.json) for the name used to invoke the script once installed.
+
+# How to work with this program
+
+- Install node, if not yet installed
+- Clone git repository
+- `cd` to the repository directory
+- Install all dependencies: `npm install`
+- Run unit tests with `npm test`
+- Install as a shell command with `npm install -g`
+- During development, link to the working version with `npm link`
+
+# License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ index.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+/**
+ * A seed to process files in JavaScript.
+ *
+ * Covers:
+ *
+ * - Process more than one file with file globbing
+ * - Working with pipes (redirect stdin and stdout)
+ * - Accepting command line options with file names
+ * - Mutually exclusive command line options with a default option
+ *
+ * Sources for this code and command-line programs in general:
+ *
+ * - https://nodejs.org/api/readline.html
+ * - https://nodejs.org/api/readline.html#readline_example_read_file_stream_line_by_line
+ * - Discussion in https://stackoverflow.com/questions/6156501/read-a-file-one-line-at-a-time-in-node-js,
+ *   especially about how **not** do it.
+ * - The command line parsing seed program in
+ *   https://github.com/cgarbin/seed-javascript-command-line-arg-parsing
+ *
+ * Examples:
+ *  - file.txt --tolower
+ *  - file.txt --toupper
+ *  - file.txt (defaults to '--tolower')
+ *  - *.txt --tolower  (file globbing)
+ *  - cat file.txt | thisscript (read from stdin)
+ *
+ * Some extended chars to test UTF-8 handling against this source code:
+ *     á Á ñ Ñ - çãü
+ */
+
+const program = require("commander");
+const readline = require("readline");
+const fs = require("fs");
+
+let files = [];
+
+program
+  .version("0.1.0")
+  .description("Converts files to all upper or all lower case.")
+  .arguments("[files...]")
+  // Option with two values and default
+  .option("-l, --tolower", "Convert to lower case (default)")
+  .option("-u, --toupper", "Convert to upper case")
+  .option("-v, --verbose", "Verbose mode")
+  .action(function(f) {
+    files = f;
+  })
+  .parse(process.argv);
+
+// Show help if no option given (also exits the program)
+if (program.args.length === 0) {
+  // TODO: change to read from stdin in this case
+  program.help();
+}
+
+// Parameter validation: both options specified
+if (program.tolower && program.toupper) {
+  console.warn("Ignoring --toupper, using --tolower");
+  program.toupper = false;
+}
+
+// Parameter validation: no option specified (use default)
+if (!program.tolower && !program.toupper) {
+  program.tolower = true;
+}
+
+// Just to make cose a bit shorter and more readable
+const lower = program.tolower;
+
+files.forEach(file => {
+  if (program.verbose) {
+    console.warn("Processing file " + file);
+  }
+
+  const rl = readline.createInterface({
+    input: file === "-" ? process.stdin : fs.createReadStream(file),
+    crlfDelay: Infinity
+  });
+  rl.on("line", line => {
+    console.log(lower ? line.toLowerCase() : line.toUpperCase());
+  });
+});
